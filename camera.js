bafd8c6986e993e082b6389ade5e0268e82e3780
alex001223 <147508297+alex001223@users.noreply.github.com>
2023-10-10 15:10:07 +0300

    Update camera.js

--- camera.js
+++ camera.js
@@ -12,3 +12,11 @@ var c = document.getElementById("canvas"); c.width = video.width;
 c.height = video.height;
 var ctx = c.getContext("2d");
 ctx.drawImage(video, 0, 0, 640, 480); }
+
+video.addEventListener('loadedmetadata', function() {
+    var c = document.getElementById("canvas");
+    c.width = video.videoWidth;
+    c.height = video.videoHeight;
+    var ctx = c.getContext("2d");
+    ctx.drawImage(video, 0, 0, c.width, c.height);
+});
