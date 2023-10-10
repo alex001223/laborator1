a2c4971e125068de5211f5af0bfab083d58aac56
alex001223 <147508297+alex001223@users.noreply.github.com>
2023-10-10 15:00:53 +0300

    Update camera.js

--- camera.js
+++ camera.js
@@ -3,11 +3,11 @@ video.srcObject = stream; }
 function on_cam_error(err)
 {
 alert("error."+err.message); }
-//-----------------------------------------------
+
 var constraints = { audio: false, video: true }; navigator.mediaDevices.getUserMedia(constraints) .then(on_cam_success)
-.catch(on_cam_error);
-function capteaza()
-{
+.catch(on_cam_error); 
+function capteaza(){
+	
 var c = document.getElementById("canvas"); c.width = video.width;
 c.height = video.height;
 var ctx = c.getContext("2d");
